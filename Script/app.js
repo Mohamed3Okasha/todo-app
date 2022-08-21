c6f72204e39e96bb35d6694050378a08c340b85e
Mohamed <mokasha810@gmail.com>
2022-08-21 06:37:45 +0200

    handle logout logic and remove todo list

--- Script/app.js
+++ Script/app.js
@@ -48,6 +48,27 @@ function handleLoginSubmit(){
   }
 }
 
+function handleLogout() {
+  const arrCookies = arrangedCookies();
+  for (let cookie in arrCookies) {
+    this.deleteCookie(cookie);
+  }
+
+  loginStatus.innerHTML = `
+  <button class="btn btn-primary">Login</button>
+  <button
+    type="button"
+    class="btn btn-secondary"
+    data-bs-toggle="modal"
+    data-bs-target="#registerModal"
+  >
+    Register
+  </button>
+    `;
+
+  removeTodoList();
+}
+
 function validateForm(emailInput, passwordInput) {
   const errors = {};
   const emailRegex = /^\w+([\.-]?\w+)*@\w+[^.]([\-]?\w+)*(\.\w{2,})+$/;
@@ -92,7 +113,14 @@ function changeLoginStatus(email){
 
 function showTodoList(){
   const todoContent = document.querySelector(".todo-content");
-  todoContent.innerHTML = `Here's your todo content`
+  todoContent.innerHTML = `Here's your todo content`;
+}
+
+function removeTodoList(){
+  const todoContent = document.querySelector(".todo-content");
+  todoContent.innerHTML = `<h3 class="mt-5">
+  Welcome to ToDo App, Please login / register to access your ToDo List
+  </h3>`;
 }
 
 function setCookie(key, val) {
